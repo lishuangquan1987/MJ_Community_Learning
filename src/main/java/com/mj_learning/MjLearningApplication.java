7a59ee4a9bebf78cd90328ec94bc6501d00647ad
lishuangquan <lishuangquan@pcitech.com>
2020-04-18 17:08:55 +0800

    初次提交

---
+++ src/main/java/com/mj_learning/MjLearningApplication.java
@@ -0,0 +1,13 @@
+package com.mj_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjLearningApplication.class, args);
+    }
+
+}
